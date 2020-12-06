a5dd4f92e22cd4d9400cf2cb852ae6c5ca2b05ab
snuckdev <joseeduardoampla@gmail.com>
2020-12-06 15:54:52 -0300

    remove shit thing

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ function saveImage(url) {
 
 function generateRandomId(length) {
   var result           = '';
-  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var characters       = 'abcdefghijklmnopqrstuvwxyz0123456789';
   var charactersLength = characters.length;
   for ( var i = 0; i < length; i++ ) {
      result += characters.charAt(Math.floor(Math.random() * charactersLength));
